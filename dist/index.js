02665b0ab5755fd27231cc52e6388e3e41366efd
Jessica Swenson <jessicamswens@gmail.com>
2023-05-10 07:40:11 -0600

    first commit..ish

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
-console.log('its working');
-console.log('its working');
-console.log('its working');
-console.log('its working');
-console.log('its working');
+
+class Player {
+    taunt() {
+        console.log('Booyah!');
+    }
+}
+
+const player1 = new Player()
+player1.taunt()
+
+const player2 = new Player()
+player2.taunt()
+
+console.log(player1);
+
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-console.log('its working');
-console.log('its working');
-console.log('its working');
-console.log('its working');
-console.log('its working');
